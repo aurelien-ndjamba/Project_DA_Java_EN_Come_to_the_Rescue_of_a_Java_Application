9dbef2230d8366b4d22d0987dcc9cb62917931da
andjamba <aurelien.ndjamba@gmail.com>
2021-01-10 14:09:40 +0100

    Ajout de la librairie 'java.util.ArrayList' + méthode abstraite 'getSymptomsWithDuplicate' + commentaire javadoc dans l'interface ISymptomReader

--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
@@ -1,5 +1,6 @@
 package com.hemebiotech.analytics;
 
+import java.util.ArrayList;
 
 /**
  * Anything that will read symptom data from a source
@@ -13,7 +14,12 @@ public interface ISymptomReader {
 	/**
 	 * If no data is available, return an empty List
 	 * 
-	 * @return a raw listing of all Symptoms obtained from a data source, duplicates are possible/probable
+	 * @return a raw listing of all Symptoms obtained from a data source, duplicates
+	 *         are possible/probable
+	 *    
+	 * @param filepath link of symptom file to analyze
+	 *    
 	 */
+public ArrayList<String> getSymptomsWithDuplicate(String filepath);
 
 }
