dce828189cd1321862f7ee3b6381e7cf02642c20
andjamba <aurelien.ndjamba@gmail.com>
2021-01-11 09:35:58 +0100

    Ajout de librairie dans la classe ReadSymptomDataFromFile.

--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -5,6 +5,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Class representing to read data from a file on disk.
