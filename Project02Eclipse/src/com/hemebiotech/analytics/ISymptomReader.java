9e73fe1e67154e2f77d12e70633e9933cef408f5
andjamba <aurelien.ndjamba@gmail.com>
2021-01-11 09:52:22 +0100

    Suppresion de la variable 'List<String> GetSymptoms ();' et de la librairie 'java.util.List' de l'interface ISymptomReader

--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
@@ -1,6 +1,6 @@
 package com.hemebiotech.analytics;
 
-import java.util.List;
+
 
 /**
  * Anything that will read symptom data from a source
@@ -11,10 +11,5 @@ import java.util.List;
  * 
  */
 public interface ISymptomReader {
-	/**
-	 * If no data is available, return an empty List
-	 * 
-	 * @return a raw listing of all Symptoms obtained from a data source, duplicates are possible/probable
-	 */
-	List<String> GetSymptoms ();
+	
 }
