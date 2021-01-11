c3334609855b5a73fcd7fda3ace6fbd3aed65fd6
andjamba <aurelien.ndjamba@gmail.com>
2021-01-11 09:26:22 +0100

    Suppression de la méthode 'GetSymptoms()' de la classe ReadSymptomDataFromFile.

--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -16,27 +16,5 @@ public class ReadSymptomDataFromFile implements ISymptomReader {
 
 	
 
-	
-	@Override
-	public List<String> GetSymptoms() {
-		ArrayList<String> result = new ArrayList<String>();
-		
-		if (filepath != null) {
-			try {
-				BufferedReader reader = new BufferedReader (new FileReader(filepath));
-				String line = reader.readLine();
-				
-				while (line != null) {
-					result.add(line);
-					line = reader.readLine();
-				}
-				reader.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		return result;
-	}
 
 }
