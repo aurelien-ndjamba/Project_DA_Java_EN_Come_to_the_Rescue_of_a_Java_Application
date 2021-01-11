02e23ce78be974f85cfde8488687c6f62a4b1f58
andjamba <aurelien.ndjamba@gmail.com>
2021-01-11 10:12:31 +0100

    Import de 'java.io.FileNotFoundException' et suppression de 'java.util.List'

--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -1,10 +1,10 @@
 package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
