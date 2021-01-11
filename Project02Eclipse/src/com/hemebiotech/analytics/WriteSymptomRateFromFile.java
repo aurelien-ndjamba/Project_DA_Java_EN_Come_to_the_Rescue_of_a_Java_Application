6a5809c752af0b4e5b14f98a081f075a1d9730f4
andjamba <aurelien.ndjamba@gmail.com>
2021-01-11 10:54:05 +0100

    Création de l'interface ISymptomWriter

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomRateFromFile.java
@@ -0,0 +1,17 @@
+package com.hemebiotech.analytics;
+
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Class ExportSymptomsRate: representing a file export in a "result.out file"
+ */
+public class WriteSymptomRateFromFile implements ISymptomWriter {
+	
+	
+	
+}
