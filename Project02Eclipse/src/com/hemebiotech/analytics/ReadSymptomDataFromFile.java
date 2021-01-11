067de57f843565e2e7f1170d34552b6166cb16d7
andjamba <aurelien.ndjamba@gmail.com>
2021-01-11 09:28:16 +0100

    Ajout de la méthode 'getSymptomsWithDuplicate(String filepath)' dans la classe ReadSymptomDataFromFile.

--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -14,7 +14,49 @@ import java.util.List;
  */
 public class ReadSymptomDataFromFile implements ISymptomReader {
 
-	
+	/**
+	 * If no data is available, return an empty List
+	 * 
+	 * @return a raw listing of all Symptoms obtained from a data source, duplicates
+	 *         are possible/probable
+	 *         
+	 * @param filepath link of symptom file to analyze
+	 */
+	@Override
+	public ArrayList<String> getSymptomsWithDuplicate(String filepath) {
+
+		ArrayList<String> listSymptomWithDuplicat = new ArrayList<String>();
+
+		if (filepath != null) {
+			
+			BufferedReader reader = null;
+			try {
+				reader = new BufferedReader(new FileReader(filepath));
+				String line = reader.readLine();
+
+				while (line != null) {
+					listSymptomWithDuplicat.add(line.toLowerCase()); // Convertion en minuscule puis ajout des maux dans la liste.
+					line = reader.readLine();
+				}
+			} 
+			catch (FileNotFoundException e) {
+				System.out.println("the named file does not exist");
+			} 
+			catch (IOException e) {
+				e.printStackTrace();
+			} 
+			finally {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}  //-> Renvoi un message d'erreur dans le bloc finally
+			}
+		}
+
+		return listSymptomWithDuplicat;
+	}
 
 
 }
