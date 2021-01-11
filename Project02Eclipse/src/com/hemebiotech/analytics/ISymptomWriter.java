525640df2e4ced0401b63071a0aab8059291d83b
andjamba <aurelien.ndjamba@gmail.com>
2021-01-11 10:45:18 +0100

    Ajout de l'interface ISymptomWriter

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomWriter.java
@@ -0,0 +1,13 @@
+package com.hemebiotech.analytics;
+
+import java.util.Map;
+
+/**
+ * Interface ISymptomWriter: Anything that will write symptoms and their
+ * occurrence analyze from a list of symptoms to a file in the directory.
+ */
+public interface ISymptomWriter {
+	
+	
+
+}
